dad5d2a6de7f5a308aa4a1d77e24a3ef99edea7b
Emma <Emma@fanap.ir>
2024-01-31 20:02:48 +0330

    add-tailwind-css-to-project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import "./styles.css";
+// import App from "./app";
 
 const rootEl = document.querySelector("#root");
 if (!rootEl) throw new Error("Cannot find root element with that id");
 const root = createRoot(rootEl);
 root.render(
   <React.StrictMode>
-    <App />
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
   </React.StrictMode>
 );

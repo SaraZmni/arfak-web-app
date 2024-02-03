4cb8931585bb1aa50a911eb003dab3975d469997
Emma <Emma@fanap.ir>
2024-02-03 12:40:07 +0330

    add-folder-structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-// import App from "./app";
+import App from "./app";
 
 const rootEl = document.querySelector("#root");
 if (!rootEl) throw new Error("Cannot find root element with that id");
 const root = createRoot(rootEl);
 root.render(
   <React.StrictMode>
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Button
-    </button>
+    <App />
   </React.StrictMode>
 );

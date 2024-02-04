c6cf7dd3aacc8afa4c901012a5011c0c1a1cde79
Emma <Emma@fanap.ir>
2024-02-04 17:04:59 +0330

    fix router bug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,9 @@ import { MainLayout } from "@components/general/layouts";
 import { FC } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./home";
+import { About } from "./about";
+import { Blog } from "./blog";
+import { Contact } from "./contact";
 
 const AppRouter: FC = () => {
   const router = createBrowserRouter([
@@ -12,6 +15,18 @@ const AppRouter: FC = () => {
         {
           path: "/",
           element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/blog",
+          element: <Blog />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
         },
       ],
     },

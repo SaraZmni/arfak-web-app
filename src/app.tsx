e282a387a8e41692fafea007c29a8c0320c460c5
Emma <Emma@fanap.ir>
2024-02-06 18:50:56 +0330

    add nav bar menu

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,6 @@ import AppRouter from "@routes/index";
 const App = () => {
   return (
     <>
-      <h1>Arfak WebSite</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
       <AppRouter />
     </>
   );

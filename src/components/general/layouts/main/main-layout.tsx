ba37588f454f02916f61c2c80762b35ea7603d4c
Emma <Emma@fanap.ir>
2024-02-17 18:32:02 +0330

    add footer to project

--- src/components/general/layouts/main/main-layout.tsx
+++ src/components/general/layouts/main/main-layout.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { Outlet } from "react-router";
 import { Navbar } from "../navbar";
+import { Footer } from "@components/general/footer";
 
 const MainLayout: FC = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
